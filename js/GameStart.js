7bfb3bf9596afdf3a7c6530addae32077e557ccf
Ishikawa <it212013@trident.ac.jp>
2023-01-21 01:00:25 +0900

    var2.6

--- js/GameStart.js
+++ js/GameStart.js
@@ -1,77 +1,59 @@
 // localStorageにセットされているすべての値を削除
 localStorage.clear();
-
-
-var elements = document.getElementsByName("hoge");
-var len = elements.length;
+var LevelOfDifficulty = document.getLevelOfDifficultyByName("LevelOfDifficulty");//htmlの難易度を取得
+var LevelLength = LevelOfDifficulty.length;//htmlの難易度を取得
 var checkValue = null;
-var level = null;
-// ボタンを押した時の処理
+var level = null;//選択された難易度を入れる変数
+//へやをつくるボタンを押した時の処理
 document.getElementById("RoomCreate").addEventListener("click", function () {
-    // ●ボタンを押されたときに選択されていたラジオボタンのvalueを取得する処理
-    for (var i = 0; i < len; i++) {
-        if (elements.item(i).checked) {
-            checkValue = elements.item(i).value;
-
+    //●ボタンを押されたときに選択されていたラジオボタンのvalueを取得する処理
+    for (var i = 0; i < LevelLength; i++) {
+        if (LevelOfDifficulty.item(i).checked) {
+            checkValue = LevelOfDifficulty.item(i).value;
             if (checkValue === "easy") {
-
                 level = "easy";
-                localStorage.setItem('level', level);
+                localStorage.setItem('level', level);//ローカルストレージに選択した難易度を格納
                 console.log(localStorage);
-
             } else if (checkValue === "normal") {
-
                 level = "normal";
-                localStorage.setItem('level', level);
+                localStorage.setItem('level', level);//ローカルストレージに選択した難易度を格納
                 console.log(localStorage);
-
             } else if (checkValue === "hard") {
-
                 level = "hard";
-                localStorage.setItem('level', level);
+                localStorage.setItem('level', level);//ローカルストレージに選択した難易度を格納
                 console.log(localStorage);
-
             }
         }
-        // ●を打っていた処理はここで終わり 
-        // すみません●で指定しているラジオボタンの処理は詳しくはよくわかってないです
+        //●を打っていた処理はここで終わり 
+        //すみません●で指定しているラジオボタンの処理は詳しくはよくわかってないです
     }
-    // RoomCreate.htmlに画面遷移
-    window.location.href = 'RoomCreate.html';
+    window.location.href = 'RoomCreate.html';//RoomCreate.html(へやをつくる)に画面遷移
 })
 
-// ボタンを押した時の処理
+//へやにはいるボタンを押した時の処理
 document.getElementById("RoomIn").addEventListener("click", function () {
-    // ●ボタンを押されたときに選択されていたラジオボタンのvalueを取得する処理
-    for (var i = 0; i < len; i++) {
-        if (elements.item(i).checked) {
-            checkValue = elements.item(i).value;
-
+    //●ボタンを押されたときに選択されていたラジオボタンのvalueを取得する処理
+    for (var i = 0; i < LevelLength; i++) {
+        if (LevelOfDifficulty.item(i).checked) {
+            checkValue = LevelOfDifficulty.item(i).value;
             if (checkValue === "easy") {
-
                 level = "easy";
-                localStorage.setItem('level', level);
+                localStorage.setItem('level', level);//ローカルストレージに選択した難易度を格納
                 console.log(localStorage);
-
             } else if (checkValue === "normal") {
-
                 level = "normal";
-                localStorage.setItem('level', level);
+                localStorage.setItem('level', level);//ローカルストレージに選択した難易度を格納
                 console.log(localStorage);
-
             } else if (checkValue === "hard") {
-
                 level = "hard";
-                localStorage.setItem('level', level);
+                localStorage.setItem('level', level);//ローカルストレージに選択した難易度を格納
                 console.log(localStorage);
-
             }
         }
-        // ●を打っていた処理はここで終わり 
-        // すみません●で指定しているラジオボタンの処理は詳しくはよくわかってないです
+        //●を打っていた処理はここで終わり 
+        //すみません●で指定しているラジオボタンの処理は詳しくはよくわかってないです
     }
-    // RoomIn.htmlに画面遷移
-    window.location.href = 'RoomIn.html';
+    window.location.href = 'RoomIn.html';//RoomIn.html(へやにはいる)に画面遷移
 })
 
 // ボタンを押した時の処理
