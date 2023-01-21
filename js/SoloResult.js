41a429c570b881d55477a9711e44273326751570
Ishikawa <it212013@trident.ac.jp>
2023-01-21 21:16:31 +0900

    var2.6

--- js/SoloResult.js
+++ js/SoloResult.js
@@ -1,25 +1,19 @@
-
+//ローカルストレージの"Score"から正解数を取得する
 const Point = localStorage.getItem("Score");
 console.log(Point);
+//htmlに"あなたのとくてん"と表示
 document.getElementById("pointtext").innerHTML = "あなたのとくてん";
-
 //ポイントを表示
 document.getElementById("point").innerHTML = Point;
 
+//もう一回が押されたときの処理
 document.getElementById("again").addEventListener("click", function () {
-
-    // SoloGame.htmlに画面遷移
+    //SoloGame.htmlに画面遷移
     window.location.href = 'SoloGame.html';
-
 });
 
+//ホームに戻るボタンが押されたときの処理
 document.getElementById("home").addEventListener("click", function () {
-
-    // GameStart.htmlに画面遷移
+    //GameStart.htmlに画面遷移
     window.location.href = 'GameStart.html';
-
-});
-
-document.getElementById("stop").addEventListener("click", function () {
-
 });
