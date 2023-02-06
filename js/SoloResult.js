2b87ceac3ad430c452506b520df6cbbdebc61cd9
Ishikawa <it212013@trident.ac.jp>
2023-02-06 16:10:49 +0900

    var5.0

--- js/SoloResult.js
+++ js/SoloResult.js
@@ -1,6 +1,9 @@
 //ローカルストレージの"Score"から正解数を取得する
 const Point = localStorage.getItem("Score");
 console.log(Point);
+//GameStartで設定した難易度をlevelとする
+level = localStorage.getItem("level");
+console.log(level);
 //htmlに"あなたのとくてん"と表示
 document.getElementById("pointtext").innerHTML = "あなたのとくてん";
 //ポイントを表示
@@ -8,8 +11,16 @@ document.getElementById("point").innerHTML = Point;
 
 //もう一回が押されたときの処理
 document.getElementById("again").addEventListener("click", function () {
-    //SoloGame.htmlに画面遷移
-    location.href = 'SoloGame.html';
+    if (level == "easy") {
+        //SoloGame.htmlに画面遷移
+        location.href = 'SoloGameEasy.html';
+    } else if (level == "normal") {
+        //SoloGame.htmlに画面遷移
+        location.href = 'SoloGameNormal.html';
+    } else if (level == "hard") {
+        //SoloGame.htmlに画面遷移
+        location.href = 'SoloGameHard.html';
+    }
 });
 
 //ホームに戻るボタンが押されたときの処理
