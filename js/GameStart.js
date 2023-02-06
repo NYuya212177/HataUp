639feb81a53e751be786ac8e57e0fb8165654dfc
Ishikawa <it212013@trident.ac.jp>
2023-02-06 16:48:22 +0900

    var5.0

--- js/GameStart.js
+++ js/GameStart.js
@@ -89,7 +89,6 @@ document.getElementById("SoloGame").addEventListener("click", function () {
                 console.log(localStorage);
             }
         }
-
         if (level === "easy") {//難易度がかんたんの時
             //SoloGameEasy.htmlに画面遷移
             window.location.href = 'SoloGameEasy.html';
